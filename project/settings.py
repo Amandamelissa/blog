7f9afd233de1d17d487b70fcf1df611178743fea
Amanda <amandamfnobre@gmail.com>
2026-02-19 17:38:15 -0300

    commit 2

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ SECRET_KEY = 'django-insecure-9eunvs2^_*sh9*ljwdf06-=2p6w-$04i%e&w%3#&)vesgkdw8i
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['blog-7qve.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
@@ -115,4 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/calculadora/static/'
-STATIC_ROOT = '/calculadora/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
