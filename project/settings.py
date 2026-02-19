c83cf6a974c64a1953acb9746e26beea6868ec10
Amanda <amandamfnobre@gmail.com>
2026-02-19 17:41:34 -0300

    commit pythonanywhere

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9eunvs2^_*sh9*ljwdf06-=2p6w-$04i%e&w%3#&)vesgkdw8i
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['amandamelissa.pythonanywhere.com''localhost', '127.0.0.1']
 
 # Application definition
 
